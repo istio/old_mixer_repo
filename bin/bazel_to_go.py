b623c9a66f2231a86f61423b2e08b940c142219c
Mandar U Jog <mjog@google.com>
2017-01-07 19:04:50 -0800

    Add mining external dir for dependencies

--- bin/bazel_to_go.py
+++ bin/bazel_to_go.py
@@ -31,6 +31,27 @@ pathmap = {
     "github.com/istio/api": "istio.io/api"
 }
 
+known_repos = {
+        "org_golang_google": "google.golang.org",
+        "com_github": "github.com",
+        "org_golang": "golang.org",
+        "in_gopkg": "gopkg.in"
+}
+
+
+# gopkg packages are of type gopkg.in/yaml.v2
+# in_gopkg_yaml_v2 
+# com_github_hashicorp_go_multierror  --> github.com/
+def repos(name):
+   for r, m in known_repos.items():
+       if name.startswith(r):
+           rname = name[(len(r)+1):]
+           fp, _, rest = rname.partition('_')
+           if r == 'in_gopkg':
+               return m + "/" + fp + "." + rest
+
+           return m + "/" + fp + "/" + rest
+
 # If we need to support more bazel functions
 # add them here
 
@@ -110,10 +131,39 @@ def bazel_to_vendor(WKSPC):
     vendor = WKSPC + "/vendor"
     genfiles = WKSPC + "/bazel-genfiles/external/"
     vlen = len(vendor)
-    for (target, linksrc) in process(workspace, external, genfiles, vendor):
+
+    links = {target: linksrc for(target, linksrc) in process(workspace, external, genfiles, vendor)}
+
+    bysrc = {}
+
+    for (target, linksrc) in links.items():
+        makelink(target, linksrc)
+        print "Vendored", linksrc, '-->', target
+        bysrc[linksrc] = target
+
+    # check other directories in external
+    # and symlink ones that were not covered thru workspace
+    print "External links:"
+    for ext_target in get_external_links(external):
+        target = external + "/" + ext_target
+        if target in links:
+            continue
+        link = repos(ext_target)
+        if not link:
+            print "Could not resolve", ext_target
+            continue
+        linksrc = vendor + "/" + link
+
+        # only make this link if we have not made it above
+        if linksrc in bysrc:
+            print "Skipping ", link
+            continue
+
         makelink(target, linksrc)
         print "Vendored", linksrc, '-->', target
 
+def get_external_links(external):
+    return [file for file in os.listdir(external) if os.path.isdir(external+"/"+file)]
 
 def main(args):
     WKSPC = os.getcwd()
