3a889c7aeaf748796ea620490d425699941ffa57
Mandar U Jog <mjog@google.com>
2017-01-13 07:43:49 -0800

    Add Config Validation and Resolve
    1. Validater uses ConfigValidater interface to validate
    and convert untyped params to typed structs. Returns a ValidatedConfig
    2. Runtime uses a validated config.
       Given a bag attributes resolves it against a validated config to a
       list of CombinedConfig

---
+++ pkg/config/runtime.go
@@ -0,0 +1,96 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"istio.io/mixer/pkg/attribute"
+	"istio.io/mixer/pkg/expr"
+)
+
+type (
+	// Runtime Represents the runtime view of the config
+	// It is prevalidated and immutable.
+	// It can be safely dispatched
+	Runtime struct {
+		Validated
+		// used to evaluate selectors
+		evaluator expr.PredicateEvaluator
+	}
+
+	// Combined config is given to aspect managers
+	Combined struct {
+		Adapter *Adapter
+		Aspect  *Aspect
+	}
+
+	AspectSet map[string]bool
+)
+
+func NewRuntime(v *Validated, evaluator expr.PredicateEvaluator) *Runtime {
+	return &Runtime{
+		Validated: *v,
+		evaluator: evaluator,
+	}
+}
+
+// Resolve returns a list of CombinedConfig  given an attribute bag
+// It will only return config from the requested set of aspects
+// For example the Check handler and Report handler will request
+// a disjoint set of aspects check: {iplistChecker, iam}, report: {Log, metrics}
+func (p *Runtime) Resolve(bag attribute.Bag, aspectSet AspectSet) ([]*Combined, error) {
+	dlist := make([]*Combined, 0, p.numAspects)
+	err := p.resolveRules(bag, aspectSet, p.serviceConfig.GetRules(), "/", &dlist)
+	return dlist, err
+}
+
+func (p *Runtime) resolveRules(bag attribute.Bag, aspectSet AspectSet, rules []*AspectRule, path string, dlist *[]*Combined) (err error) {
+
+	var selected bool
+
+	for _, rule := range rules {
+		if selected, err = p.evaluator.EvalPredicate(rule.GetSelector(), bag); err != nil {
+			return err
+		}
+
+		if !selected {
+			continue
+		}
+		path = path + "/" + rule.GetSelector()
+		for _, aa := range rule.GetAspects() {
+			var adp *Adapter
+			if aspectSet[aa.GetKind()] {
+				// find matching adapter
+				// assume that config references are correct
+				if aa.GetAdapter() != "" {
+					adp = p.adapterByName[aa.GetAdapter()]
+				} else {
+					adp = p.adapterByKind[aa.GetKind()][0]
+				}
+				*dlist = append(*dlist, &Combined{
+					Adapter: adp,
+					Aspect:  aa,
+				})
+			}
+		}
+		rs := rule.GetRules()
+		if len(rs) == 0 {
+			continue
+		}
+		if err = p.resolveRules(bag, aspectSet, rs, path, dlist); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ pkg/config/validater.go
@@ -0,0 +1,216 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/mitchellh/mapstructure"
+	"gopkg.in/yaml.v2"
+
+	"istio.io/mixer/pkg/aspect"
+	"istio.io/mixer/pkg/expr"
+)
+
+type (
+	// ValidaterFinder is used to find specific underlying validaters
+	// Manager registry and adpater registry should implement this interface
+	// so ConfigValidaters can be uniformely accessed
+	ValidaterFinder interface {
+		Get(name string) (aspect.ConfigValidater, bool)
+	}
+)
+
+func NewValidater(managerFinder ValidaterFinder, adapterFinder ValidaterFinder, strict bool, exprValidater expr.Validater) *validator {
+	return &validator{
+		managerFinder: managerFinder,
+		adapterFinder: adapterFinder,
+		strict:        strict,
+		exprValidater: exprValidater,
+		validated:     &Validated{},
+	}
+}
+
+type (
+	validator struct {
+		managerFinder ValidaterFinder
+		adapterFinder ValidaterFinder
+		strict        bool
+		exprValidater expr.Validater
+		validated     *Validated
+	}
+
+	Validated struct {
+		// Names are given to specific adapter configurations: unique
+		adapterByName map[string]*Adapter
+		// This is more often used to match adapters
+		adapterByKind map[string][]*Adapter
+		serviceConfig *ServiceConfig
+		numAspects    int
+	}
+)
+
+// ValidateAdapterConfig consumes a yml config string with adapter config
+// It is validated in presence of
+func (p *validator) validateGlobalConfig(cfg string) (ce *aspect.ConfigErrors) {
+	m := &GlobalConfig{}
+	err := yaml.Unmarshal([]byte(cfg), m)
+	if err != nil {
+		ce = ce.Append("AdapterConfig", err)
+		return
+	}
+	p.validated.adapterByKind = make(map[string][]*Adapter)
+	p.validated.adapterByName = make(map[string]*Adapter)
+	var acfg proto.Message
+	var aArr []*Adapter
+	var found bool
+	for _, aa := range m.GetAdapters() {
+		if acfg, err = ConvertParams(p.adapterFinder, aa.Impl, aa.Params, p.strict); err != nil {
+			ce = ce.Append("Adapter: "+aa.Impl, err)
+			continue
+		}
+		aa.Params = acfg
+
+		if aArr, found = p.validated.adapterByKind[aa.GetKind()]; !found {
+			aArr = []*Adapter{}
+		}
+		aArr = append(aArr, aa)
+		p.validated.adapterByKind[aa.GetKind()] = aArr
+		if aa.GetName() != "" {
+			p.validated.adapterByName[aa.GetName()] = aa
+		}
+
+	}
+	return
+}
+
+// ValidateSelector ensures that the selector is valid per expression language
+func (p *validator) validateSelector(selector string) (err error) {
+	if len(selector) == 0 {
+		return fmt.Errorf("Empty selector not allowed")
+	}
+	return p.exprValidater.Validate(selector)
+}
+
+// ValidateRules  processes Aspect Rules
+func (p *validator) ValidateRules(rules []*AspectRule, path string, validatePresence bool) (ce *aspect.ConfigErrors) {
+	var acfg proto.Message
+	var err error
+	for _, rule := range rules {
+		if err = p.validateSelector(rule.GetSelector()); err != nil {
+			ce = ce.Append(path+":Selector "+rule.GetSelector(), err)
+		}
+		path = path + "/" + rule.GetSelector()
+		for idx, aa := range rule.GetAspects() {
+			if acfg, err = ConvertParams(p.managerFinder, aa.GetKind(), aa.GetParams(), p.strict); err != nil {
+				ce = ce.Append(fmt.Sprintf("%s:%s[%d]", path, aa.Kind, idx), err)
+				continue
+			}
+			aa.Params = acfg
+			p.validated.numAspects++
+			if validatePresence {
+				// ensure that aa.Kind has a registered adapter
+				if p.validated.adapterByKind[aa.GetKind()] == nil {
+					ce = ce.Appendf("Kind", "No adapter for Kind=%s is available", aa.GetKind())
+				}
+				if aa.GetAdapter() != "" && p.validated.adapterByName[aa.GetAdapter()] == nil {
+					ce = ce.Appendf("NamedAdapter", "No adapter by name %s Available", aa.GetAdapter())
+				}
+			}
+		}
+		rs := rule.GetRules()
+		if len(rs) == 0 {
+			continue
+		}
+		if verr := p.ValidateRules(rs, path, validatePresence); verr != nil {
+			ce = ce.Extend(verr)
+		}
+	}
+	return ce
+}
+
+// Validate a single serviceConfig and globalConfig together
+// returns a fully validated runtimeConfig if no errors are found
+func (p *validator) Validate(serviceCfg string, globalCfg string) (rt *Validated, ce *aspect.ConfigErrors) {
+
+	if re := p.validateGlobalConfig(globalCfg); re != nil {
+		return rt, ce.Append("GlobalConfig", re)
+	}
+	// The order is important here, because serviceConfig refers to global config
+
+	if re := p.validateServiceConfig(serviceCfg, true); re != nil {
+		return rt, ce.Append("ServiceConfig", re)
+	}
+
+	return p.validated, nil
+}
+
+// ValidateServiceConfig validates service config
+// if validatePresence is true it will ensure that the named adapter and Kinds
+// have an available and configured adapter
+func (p *validator) validateServiceConfig(cfg string, validatePresence bool) (ce *aspect.ConfigErrors) {
+	m := &ServiceConfig{}
+	err := yaml.Unmarshal([]byte(cfg), m)
+	if err != nil {
+		ce = ce.Append("ServiceConfig", err)
+		return
+	}
+	if ce = p.ValidateRules(m.GetRules(), "", validatePresence); ce != nil {
+		return ce
+	}
+	p.validated.serviceConfig = m
+	return
+}
+
+// UnknownValidater returns error for the given name
+func UnknownValidater(name string) error {
+	return fmt.Errorf("Unknown type [%s]", name)
+}
+
+// ConvertParams converts returns a typed protomessage based on available Validator
+func ConvertParams(finder ValidaterFinder, name string, params interface{}, strict bool) (proto.Message, error) {
+	avl, found := finder.Get(name)
+	if !found {
+		return nil, UnknownValidater(name)
+	}
+
+	acfg := avl.DefaultConfig()
+	if err := Decode(params, acfg, strict); err != nil {
+		return nil, err
+	}
+	if verr := avl.ValidateConfig(acfg); verr != nil {
+		return nil, verr
+	}
+	return acfg, nil
+}
+
+// Decode interprets src interface{} as the specified proto message
+func Decode(src interface{}, dest proto.Message, strict bool) (err error) {
+	var md mapstructure.Metadata
+	mcfg := mapstructure.DecoderConfig{
+		Metadata: &md,
+		Result:   dest,
+	}
+	var decoder *mapstructure.Decoder
+	if decoder, err = mapstructure.NewDecoder(&mcfg); err != nil {
+		return err
+	}
+	if err = decoder.Decode(src); err == nil && strict && len(md.Unused) > 0 {
+		return fmt.Errorf("Unused fields while parsing %s", md.Unused)
+	}
+
+	return err
+}
