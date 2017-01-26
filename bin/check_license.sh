87c07158b937bb25bc4e0bfcdd45216d529b08d4
Mandar U Jog <mjog@google.com>
2017-01-26 07:29:03 -0800

    remove requirement for Google copyright, move call in run_linters

--- bin/check_license.sh
+++ bin/check_license.sh
@@ -14,7 +14,7 @@ for fn in $(find cmd pkg adapter -name '*.go'); do
 		ret=$(($ret+1))
 	fi
 
-	head -20 $fn | grep Copyright | grep Google > /dev/null
+	head -20 $fn | grep Copyright > /dev/null
 	if [[ $? -ne 0 ]]; then
 		echo "${fn} missing Copyright"
 		ret=$(($ret+1))
--- bin/linters.sh
+++ bin/linters.sh
@@ -52,6 +52,7 @@ run_linters() {
     #
     # This doesn't work with our source tree for some reason, it can't find vendored imports
     # --enable=gotype\
+    $SCRIPTPATH/check_license.sh
 }
 
 set -e
@@ -64,6 +65,5 @@ prep_linters
 
 run_linters
 
-$SCRIPTPATH/check_license.sh
 
 echo Done running linters
