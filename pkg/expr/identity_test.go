e3bf6ea79f1c97c24f87c9e4ed541ae6677a6e69
Mandar U Jog <mjog@google.com>
2017-01-31 20:01:57 -0800

    Add support for simple evalPredicate
    Single attribute is evaluated for equality
    '$' is used as an attribute designator
    
    selector: $service_name == abcd
    or
    selector: $request_status_code == 200

--- pkg/expr/identity_test.go
+++ pkg/expr/identity_test.go
@@ -23,6 +23,8 @@ import (
 	"bytes"
 	"time"
 
+	"strings"
+
 	mixerpb "istio.io/api/mixer/v1"
 	"istio.io/mixer/pkg/attribute"
 )
@@ -143,38 +145,73 @@ func TestEvalPredicate(t *testing.T) {
 		attrs *mixerpb.Attributes
 		in    string
 		out   interface{}
-		err   bool
+		err   string
 	}{
 		// We cannot test ByteAttributes here because you cannot compare []byte using '==' or '!='
 		{attrs: &mixerpb.Attributes{
 			Dictionary:       dictionary{1: "foo", 2: "bar"},
 			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
-		}, in: "bar", out: "bar", err: true},
-		{attrs: &mixerpb.Attributes{Dictionary: dictionary{4: "i64"}, Int64Attributes: map[int32]int64{4: 37}}, in: "i64", out: int64(0), err: true},
-		{attrs: &mixerpb.Attributes{Dictionary: dictionary{17: "dbl"}, DoubleAttributes: map[int32]float64{17: 5.9}}, in: "dbl", out: 0, err: true},
+		}, in: "badExpr1", out: "bar", err: "invalid expression"},
 		{attrs: &mixerpb.Attributes{
-			Dictionary:       dictionary{0: "bool", 1: "str"},
-			BoolAttributes:   map[int32]bool{0: true},
-			StringAttributes: map[int32]string{1: "foo"},
-		}, in: "bool", out: true, err: false},
-		{attrs: &mixerpb.Attributes{Dictionary: dictionary{5: "time"}, TimestampAttributes: map[int32]*ts.Timestamp{5: ts1}}, in: "time", out: nil, err: true},
-		{attrs: &mixerpb.Attributes{Dictionary: dictionary{2: "a"}, BoolAttributes: map[int32]bool{2: true}}, in: "b", out: false, err: true},
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "badExpr2==", out: false, err: ""},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "$badExpr3==", out: "bar", err: "unresolved attribute"},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "==$badExpr3", out: "bar", err: "unresolved attribute"},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "==$", out: "bar", err: "empty attribute name"},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "$bar==bar", out: true, err: ""},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "$bar==foo", out: false, err: ""},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "$bar==foo", out: false, err: ""},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "bar"},
+		}, in: "$bar == $foo", out: false, err: ""},
+		{attrs: &mixerpb.Attributes{
+			Dictionary:       dictionary{1: "foo", 2: "bar"},
+			StringAttributes: map[int32]string{1: "foo", 2: "foo"},
+		}, in: "$bar == $foo", out: true, err: ""},
 	}
-	for _, c := range cases {
+	for idx, c := range cases {
 		bag, err := attribute.NewManager().NewTracker().StartRequest(c.attrs)
 		if err != nil {
 			t.Errorf("Failed to create bag for case: %v; err: %v", c, err)
 		}
 		id := NewIdentityEvaluator()
+		if err = id.Validate(c.in); err != nil {
+			t.Errorf("[%d] Unexpected error %s", idx, err.Error())
+		}
 		val, err := id.EvalPredicate(c.in, bag)
-		if c.err && err == nil {
-			t.Errorf("Expected err on case '%+v' but got none.", c)
+		if c.err != "" && err == nil {
+			t.Errorf("[%d] Expected err on case '%+v' but got none. val=#%v", idx, c, val)
 		}
-		if !c.err && err != nil {
-			t.Errorf("Failed to eval mapExpression '%s' in bag: %v; with err: %v", c.in, bag, err)
+		if c.err == "" && err != nil {
+			t.Errorf("[%d] Failed to eval mapExpression '%s' in bag: %v; with err: %v", idx, c.in, bag, err)
 		}
-		if !c.err && val != c.out {
-			t.Errorf("Expected val '%v' for key '%s', actual '%v'", c.out, c.in, val)
+		if c.err != "" && err != nil {
+			if !strings.Contains(err.Error(), c.err) {
+				t.Errorf("[%d] Got :%s\nWant: %s", idx, err, c.err)
+			}
+		}
+		if c.err == "" && val != c.out {
+			t.Errorf("[%d] Expected val '%v' for key '%s', actual '%v'", idx, c.out, c.in, val)
 		}
 	}
 }
