7d909242c2a5b3e85a2dcfc87b11584f54235098
Douglas Reid <dougreid@google.com>
2017-02-07 09:44:02 -0800

    Switch to using DefaultRegistry and DefaultGatherer.

--- pkg/adapter/metrics.go
+++ pkg/adapter/metrics.go
@@ -73,7 +73,7 @@ type (
 		MetricValue interface{}
 	}
 
-	// Kind defines the set of known metrics types that can be generated
+	// MetricKind defines the set of known metrics types that can be generated
 	// by the mixer.
 	MetricKind int
 
