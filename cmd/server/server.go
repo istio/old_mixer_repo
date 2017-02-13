42999316555179228f7a7169432b1650a9f1441e
Mandar U Jog <mjog@google.com>
2017-02-12 20:10:45 -0800

    Add explanatory comments on parser and funcs

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -134,7 +134,7 @@ func serverOpts(sa *serverArgs, handlers api.Handler) (*api.GRPCServerOptions, e
 
 func runServer(sa *serverArgs) error {
 	// get aspect registry with proper aspect --> api mappings
-	eval := expr.NewIdentityEvaluator()
+	eval := expr.NewCexlEvaluator()
 	adapterMgr := adapterManager.NewManager(adapter.Inventory(), aspect.Inventory(), eval)
 	configManager := config.NewManager(eval, adapterMgr.AspectValidatorFinder(), adapterMgr.BuilderValidatorFinder(),
 		sa.globalConfigFile, sa.serviceConfigFile, time.Second*time.Duration(sa.configFetchIntervalSec))
