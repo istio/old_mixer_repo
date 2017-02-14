719b3b49d30f0c9421f73a57c0a1d52990bed33a
Mandar U Jog <mjog@google.com>
2017-02-13 19:14:45 -0800

    deal with spaces correctly

--- bin/fmt.sh
+++ bin/fmt.sh
@@ -14,12 +14,11 @@ UX=$(uname)
 #remove blank lines so gofmt / goimports can do their job
 for fl in ${GO_FILES}; do
 	if [[ ${UX} == "Darwin" ]];then
-		sed -i '' -e '/^import\s*(/,/)/{ /^\s*$/d;}' $fl
+		sed -i '' -e "/^import[[:space:]]*(/,/)/{ /^\s*$/d;}" $fl
 	else
-		sed -i -e '/^import\s*(/,/)/{ /^\s*$/d;}' $fl
+		sed -i -e "/^import[[:space:]]*(/,/)/{ /^\s*$/d;}" $fl
 	fi
 done
-
 gofmt -s -w ${GO_FILES}
 goimports -w -local istio.io ${GO_FILES}
 buildifier -mode=fix $(find adapter cmd pkg -name BUILD -type f)
