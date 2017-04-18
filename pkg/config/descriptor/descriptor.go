02e6561ccf34d2f2db09db3e07199d6fda78da9e
Mandar U Jog <mjog@google.com>
2017-04-18 14:42:59 -0700

    Add granular parsing

--- pkg/config/descriptor/descriptor.go
+++ pkg/config/descriptor/descriptor.go
@@ -15,7 +15,18 @@
 package descriptor
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/ghodss/yaml"
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/glog"
+
 	dpb "istio.io/api/mixer/v1/config/descriptor"
+	"istio.io/mixer/pkg/adapter"
 	pb "istio.io/mixer/pkg/config/proto"
 	"istio.io/mixer/pkg/expr"
 )
@@ -47,6 +58,105 @@ type finder struct {
 	principals         map[string]*dpb.PrincipalDescriptor
 	quotas             map[string]*dpb.QuotaDescriptor
 	attributes         map[string]*dpb.AttributeDescriptor
+}
+
+type newMsg func() (message proto.Message)
+
+var typeMap = map[string]newMsg{
+	"logs":               func() proto.Message { return &dpb.LogEntryDescriptor{} },
+	"metrics":            func() proto.Message { return &dpb.MetricDescriptor{} },
+	"monitoredResources": func() proto.Message { return &dpb.MonitoredResourceDescriptor{} },
+	"principals":         func() proto.Message { return &dpb.PrincipalDescriptor{} },
+	"quotas":             func() proto.Message { return &dpb.QuotaDescriptor{} },
+	"manifests":          func() proto.Message { return &dpb.AttributeDescriptor{} },
+}
+
+// Parse parses a descriptor config in its parts.
+func Parse(cfg string) (dcfg *pb.GlobalConfig, ce *adapter.ConfigErrors) {
+	m := map[string]interface{}{}
+	var err error
+
+	if err = yaml.Unmarshal([]byte(cfg), &m); err != nil {
+		return nil, ce.Append("DescriptorConfig", err)
+	}
+	dcfg = &pb.GlobalConfig{}
+	var cerr *adapter.ConfigErrors
+	var oarr interface{}
+
+	//flatten manifest
+	k := "manifests"
+	val := m[k]
+	if val != nil {
+		mani := []*pb.AttributeManifest{}
+		for _, msft := range val.([]interface{}) {
+			manifest := msft.(map[string]interface{})
+			attr := manifest["attributes"].([]interface{})
+			if oarr, cerr = processArray(fmt.Sprintf("%s/%v", k, manifest["name"]), attr, typeMap[k]); cerr != nil {
+				ce = ce.Extend(cerr)
+				continue
+			}
+
+			mani = append(mani, &pb.AttributeManifest{
+				Attributes: oarr.([]*dpb.AttributeDescriptor),
+			})
+		}
+		dcfg.Manifests = mani
+	}
+
+	for k, kfn := range typeMap {
+		if k == "manifests" {
+			continue
+		}
+		val = m[k]
+		if val == nil {
+			if glog.V(2) {
+				glog.Warningf("%s missing", k)
+			}
+			continue
+		}
+
+		if oarr, cerr = processArray(k, val.([]interface{}), kfn); cerr != nil {
+			ce = ce.Extend(cerr)
+			continue
+		}
+		switch k {
+		case "logs":
+			dcfg.Logs = oarr.([]*dpb.LogEntryDescriptor)
+		case "metrics":
+			dcfg.Metrics = oarr.([]*dpb.MetricDescriptor)
+		case "quotas":
+			dcfg.Quotas = oarr.([]*dpb.QuotaDescriptor)
+		}
+	}
+
+	return
+}
+
+// processArray and return typed array
+func processArray(name string, arr []interface{}, newMessage newMsg) (interface{}, *adapter.ConfigErrors) {
+	var enc []byte
+	var err error
+	var ce *adapter.ConfigErrors
+	nm := newMessage()
+	ptrType := reflect.TypeOf(nm)
+	valType := (reflect.Indirect(reflect.ValueOf(nm))).Type()
+	outarr := reflect.MakeSlice(reflect.SliceOf(ptrType), 0, len(arr))
+	for idx, attr := range arr {
+		if enc, err = json.Marshal(attr); err != nil {
+			ce = ce.Append(fmt.Sprintf("%s[%d]", name, idx), err)
+			continue
+		}
+		dm := reflect.New(valType).Elem().Addr().Interface().(proto.Message)
+
+		if err = jsonpb.Unmarshal(bytes.NewReader(enc), dm); err != nil {
+			ce = ce.Append(fmt.Sprintf("%s[%d]", name, idx), fmt.
+				Errorf("%s: %s", err.Error(), fmt.Sprintf(string(enc))))
+			continue
+
+		}
+		outarr = reflect.Append(outarr, reflect.ValueOf(dm))
+	}
+	return outarr.Interface(), ce
 }
 
 // NewFinder constructs a new Finder for the provided global config.
