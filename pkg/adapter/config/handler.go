9331c8bda783d3c4cd1b86a3a30adb31a5dd9eb3
guptasu <guptasu@google.com>
2017-06-20 11:54:55 -0700

    Refactor handler interface.

--- pkg/adapter/config/handler.go
+++ pkg/adapter/config/handler.go
@@ -6,20 +6,27 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-// Handler represents default functionality every Adapter must implement.
-type Handler interface {
-	io.Closer
+type (
+	// Handler represents default functionality every Adapter must implement.
+	Handler interface {
+		io.Closer
+		AdapterConfigValidatorAndConfigurer
 
-	// Name returns the official name of the aspects produced by this builder.
-	Name() string
-	// Description returns a user-friendly description of the aspects produced by this builder.
-	Description() string
-	// DefaultConfig returns a default configuration struct for this
-	// adapter. This will be used by the configuration system to establish
-	// the shape of the block of configuration state passed to the Configure method.
-	DefaultConfig() proto.Message
-	// ValidateConfig determines whether the given configuration meets all correctness requirements.
-	ValidateConfig(proto.Message) error
-	// Configure is invoked by Mixer to pass an instance of the default configuration to the adapter implementation.
-	Configure(proto.Message) error
-}
+		// Name returns the official name of the aspects produced by this builder.
+		Name() string
+		// Description returns a user-friendly description of the aspects produced by this builder.
+		Description() string
+	}
+
+	// ConfigValidator handles adapter configuration defaults and validation.
+	AdapterConfigValidatorAndConfigurer interface {
+		// DefaultConfig returns a default configuration struct for this
+		// adapter. This will be used by the configuration system to establish
+		// the shape of the block of configuration state passed to the Configure method.
+		DefaultConfig() proto.Message
+		// ValidateConfig determines whether the given configuration meets all correctness requirements.
+		ValidateConfig(proto.Message) error
+		// Configure is invoked by Mixer to pass an instance of the default configuration to the adapter implementation.
+		Configure(proto.Message) error
+	}
+)
