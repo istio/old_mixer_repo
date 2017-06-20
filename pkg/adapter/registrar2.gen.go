6bbe6a94169f07f1fd972be42684f0d43b858f9b
guptasu <guptasu@google.com>
2017-06-20 11:01:55 -0700

    Name the imports in generated file. This is done so that goimports tool does not delete the imports.

--- pkg/adapter/registrar2.gen.go
+++ pkg/adapter/registrar2.gen.go
@@ -14,11 +14,13 @@
 
 package adapter
 
-import "istio.io/mixer/pkg/templates/sample/report"
+import (
+	sample_report "istio.io/mixer/pkg/templates/sample/report"
+)
 
 // Registrar2 is used by adapters to register themselves as processors of one or more available templates.
 type Registrar2 interface {
-	RegisterSampleProcessor(processor istio_mixer_adapter_sample_report.SampleProcessor)
+	RegisterSampleProcessor(processor sample_report.SampleProcessor)
 }
 
 // RegisterFn2 is a function Mixer invokes to trigger adapters to register
