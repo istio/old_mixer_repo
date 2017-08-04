e4d0aa937ecd5dcfa79652c448976b989abb86df
Sebastien Vas <sebastienvas@google.com>
2017-08-03 19:59:12 -0700

    Fix postsubmit as well

--- prow/mixer-postsubmit.sh
+++ prow/mixer-postsubmit.sh
@@ -29,7 +29,7 @@ set -x
 if [ "${CI:-}" == 'bootstrap' ]; then
     # Test harness will checkout code to directory $GOPATH/src/github.com/istio/istio
     # but we depend on being at path $GOPATH/src/istio.io/istio for imports.
-    ln -s ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
+    mv ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
     cd "${GOPATH}/src/istio.io/mixer"
 
     # Use the provided base sha, from prow.
