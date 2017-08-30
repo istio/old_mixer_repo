6d12f8cd5826e19108f07f6ea480ca3ddf233819
ozben <ozben@google.com>
2017-08-30 08:16:58 -0700

    Fix the concurrency issue in evaluator.IL.
    Mainly fixing two issues:
    - The write to the finder pointer is not guaranteed to be atomic.
    - Due to pointer capture in a local during the compilation, it was possible
      to add an interpreter to the cache that is based on the old version
      of the metadata.

--- pkg/il/evaluator/evaluator_test.go
+++ pkg/il/evaluator/evaluator_test.go
@@ -110,7 +110,7 @@ func TestEvalPredicate_WrongType(t *testing.T) {
 
 func TestEvalType(t *testing.T) {
 	e := initEvaluator(t, configBool)
-	ty, err := e.EvalType("attr", e.finder)
+	ty, err := e.EvalType("attr", e.getAttrContext().finder)
 	if err != nil {
 		t.Fatalf("error: %s", err)
 	}
@@ -121,7 +121,7 @@ func TestEvalType(t *testing.T) {
 
 func TestEvalType_WrongType(t *testing.T) {
 	e := initEvaluator(t, configBool)
-	_, err := e.EvalType("boo", e.finder)
+	_, err := e.EvalType("boo", e.getAttrContext().finder)
 	if err == nil {
 		t.Fatal("Was expecting an error")
 	}
@@ -129,7 +129,7 @@ func TestEvalType_WrongType(t *testing.T) {
 
 func TestAssertType(t *testing.T) {
 	e := initEvaluator(t, configBool)
-	err := e.AssertType("attr", e.finder, pbv.BOOL)
+	err := e.AssertType("attr", e.getAttrContext().finder, pbv.BOOL)
 	if err != nil {
 		t.Fatalf("error: %s", err)
 	}
@@ -137,7 +137,7 @@ func TestAssertType(t *testing.T) {
 
 func TestAssertType_WrongType(t *testing.T) {
 	e := initEvaluator(t, configBool)
-	err := e.AssertType("attr", e.finder, pbv.STRING)
+	err := e.AssertType("attr", e.getAttrContext().finder, pbv.STRING)
 	if err == nil {
 		t.Fatal("Was expecting an error")
 	}
@@ -145,7 +145,7 @@ func TestAssertType_WrongType(t *testing.T) {
 
 func TestAssertType_EvaluationError(t *testing.T) {
 	e := initEvaluator(t, configBool)
-	err := e.AssertType("boo", e.finder, pbv.BOOL)
+	err := e.AssertType("boo", e.getAttrContext().finder, pbv.BOOL)
 	if err == nil {
 		t.Fatal("Was expecting an error")
 	}
@@ -163,7 +163,7 @@ func TestConfigChange(t *testing.T) {
 
 	f := descriptor.NewFinder(&configBool)
 	e.ConfigChange(nil, f, nil)
-	if e.finder != f {
+	if e.getAttrContext().finder != f {
 		t.Fatal("Finder is not set correctly")
 	}
 
