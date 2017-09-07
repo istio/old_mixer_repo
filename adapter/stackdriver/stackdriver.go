00aab7686199d3fca8b97adeaf6750676daeb130
Zack Butcher <zbutcher@google.com>
2017-09-07 13:40:32 -0700

    Fix bugs in Stackdriver implementation and correct configs.
    The version of the Stackdriver Logging PR was missing some commits (turns
    out I had split them off to their own branch but I thought I'd made them
    in the PR branch).
    
    We correct the configuration for Stackdriver so it actually works, and
    introduce new Stackdriver specific metrics filling in the monitored
    resource fields.
    
    I've verified both metrics and logs are correctly produced and ingested
    by the Stackdriver APIs using these configs.

--- adapter/stackdriver/stackdriver.go
+++ adapter/stackdriver/stackdriver.go
@@ -17,7 +17,7 @@ package stackdriver
 import (
 	"context"
 
-	multierror "github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-multierror"
 
 	"istio.io/mixer/adapter/stackdriver/config"
 	"istio.io/mixer/adapter/stackdriver/log"
