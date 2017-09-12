ae296cb4b388ff981e57cd56555912d547362348
Sunny Gupta <guptasu@google.com>
2017-09-12 11:42:50 -0700

    remove _ (underscores)

--- test/spyAdapter/spyadapter.go
+++ test/spyAdapter/spyadapter.go
@@ -29,43 +29,38 @@ type (
 
 	// Adapter is a fake Adapter. It is used for controlling the Adapter's behavior as well as
 	// inspect the input values that adapter receives from Mixer
-	// nolint
 	Adapter struct {
 		Behavior    AdapterBehavior
 		BuilderData *builderData
 		HandlerData *handlerData
 	}
 
-	// nolint
 	AdapterBehavior struct {
 		Name    string
 		Builder BuilderBehavior
 		Handler HandlerBehavior
 	}
 
-	// nolint
 	HandlerBehavior struct {
-		HandleSampleReport_Error error
-		HandleSampleReport_Panic bool
+		HandleSampleReportErr   error
+		HandleSampleReportPanic bool
 
-		Close_Error error
-		Close_Panic bool
+		CloseErr   error
+		ClosePanic bool
 	}
 
-	// nolint
 	BuilderBehavior struct {
-		SetSampleReportTypes_Panic bool
+		SetSampleReportTypesPanic bool
 
-		SetAdapterConfig_Panic bool
+		SetAdapterConfigPanic bool
 
-		Validate_Err   *adapter.ConfigErrors
-		Validate_Panic bool
+		ValidateErr   *adapter.ConfigErrors
+		ValidatePanic bool
 
-		Build_Err   error
-		Build_Panic bool
+		BuildErr   error
+		BuildPanic bool
 	}
 
-	// nolint
 	builder struct {
 		behavior        BuilderBehavior
 		handlerBehavior HandlerBehavior
@@ -73,94 +68,91 @@ type (
 		handlerData     *handlerData
 	}
 
-	// nolint
 	handler struct {
 		behavior HandlerBehavior
 		data     *handlerData
 	}
 
-	// nolint
 	handlerData struct {
-		HandleSampleReport_Instances []*reportTmpl.Instance
-		HandleSampleReport_Count     int
+		HandleSampleReportInstances []*reportTmpl.Instance
+		HandleSampleReportCount     int
 
-		Close_Count int
+		CloseCount int
 	}
 
-	// nolint
 	builderData struct {
 		// no of time called
-		SetSampleReportTypes_Count int
+		SetSampleReportTypesCount int
 		// input to the method
-		SetSampleReportTypes_Types map[string]*reportTmpl.Type
+		SetSampleReportTypesTypes map[string]*reportTmpl.Type
 
-		SetAdapterConfig_AdptCfg adapter.Config
-		SetAdapterConfig_Count   int
+		SetAdapterConfigAdptCfg adapter.Config
+		SetAdapterConfigCount   int
 
-		Validate_Count int
+		ValidateCount int
 
-		Build_Count int
-		Build_Ctx   context.Context
-		Build_Env   adapter.Env
+		BuildCount int
+		BuildCtx   context.Context
+		BuildEnv   adapter.Env
 	}
 )
 
 func (b builder) Build(ctx context.Context, env adapter.Env) (adapter.Handler, error) {
-	b.data.Build_Count++
-	if b.behavior.Build_Panic {
+	b.data.BuildCount++
+	if b.behavior.BuildPanic {
 		panic("Build")
 	}
 
-	b.data.Build_Ctx = ctx
-	b.data.Build_Env = env
+	b.data.BuildCtx = ctx
+	b.data.BuildEnv = env
 
-	return handler{behavior: b.handlerBehavior, data: b.handlerData}, b.behavior.Build_Err
+	return handler{behavior: b.handlerBehavior, data: b.handlerData}, b.behavior.BuildErr
 }
 
 func (b builder) SetSampleReportTypes(typeParams map[string]*reportTmpl.Type) {
-	b.data.SetSampleReportTypes_Count++
-	b.data.SetSampleReportTypes_Types = typeParams
+	b.data.SetSampleReportTypesCount++
+	b.data.SetSampleReportTypesTypes = typeParams
 
-	if b.behavior.SetSampleReportTypes_Panic {
+	if b.behavior.SetSampleReportTypesPanic {
 		panic("SetSampleReportTypes")
 	}
 }
 
 func (b builder) SetAdapterConfig(cfg adapter.Config) {
-	b.data.SetAdapterConfig_Count++
-	b.data.SetAdapterConfig_AdptCfg = cfg
+	b.data.SetAdapterConfigCount++
+	b.data.SetAdapterConfigAdptCfg = cfg
 
-	if b.behavior.SetAdapterConfig_Panic {
+	if b.behavior.SetAdapterConfigPanic {
 		panic("SetAdapterConfig")
 	}
 }
 
 func (b builder) Validate() *adapter.ConfigErrors {
-	b.data.Validate_Count++
-	if b.behavior.Validate_Panic {
+	b.data.ValidateCount++
+	if b.behavior.ValidatePanic {
 		panic("Validate")
 	}
 
-	return b.behavior.Validate_Err
+	return b.behavior.ValidateErr
 }
 
 func (h handler) HandleSampleReport(ctx context.Context, instances []*reportTmpl.Instance) error {
-	h.data.HandleSampleReport_Count++
-	if h.behavior.HandleSampleReport_Panic {
+	h.data.HandleSampleReportCount++
+	if h.behavior.HandleSampleReportPanic {
 		panic("HandleSampleReport")
 	}
 
-	h.data.HandleSampleReport_Instances = instances
-	return h.behavior.HandleSampleReport_Error
+	h.data.HandleSampleReportInstances = instances
+	return h.behavior.HandleSampleReportErr
 }
 
 func (h handler) Close() error {
-	h.data.Close_Count++
-	if h.behavior.Close_Panic {
+	h.data.CloseCount++
+	if h.behavior.ClosePanic {
 		panic("Close")
 	}
 
-	return h.behavior.Close_Error
+	return h.behavior.CloseErr
 }
 
 // NewSpyAdapter returns a new instance of Adapter with the given behavior
