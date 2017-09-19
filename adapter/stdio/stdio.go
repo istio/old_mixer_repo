d371d5034166606b52757ca9237c6066d1aa4954
Douglas Reid <dougreid@google.com>
2017-09-18 21:38:28 -0700

    Update metrics shapes

--- adapter/stdio/stdio.go
+++ adapter/stdio/stdio.go
@@ -58,7 +58,17 @@ func (h *handler) HandleLogEntry(_ context.Context, instances []*logentry.Instan
 
 		for _, varName := range h.logEntryVars[instance.Name] {
 			if value, ok := instance.Variables[varName]; ok {
-				fields = append(fields, field(varName, value))
+				// TODO: remove when IP_ADDRESS is properly handled by Mixer
+				switch value.(type) {
+				case []byte:
+					b := value.([]byte)
+					if len(b) == net.IPv4len || len(b) == net.IPv6len {
+						fields = append(fields, zap.Any(varName, net.IP(b)))
+					}
+					continue
+				default:
+					fields = append(fields, zap.Any(varName, value))
+				}
 			}
 		}
 
